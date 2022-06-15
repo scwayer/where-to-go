1cb397a375d281e49b008c046ddc72848b6aaded
scwayer <troyanskiy-a@mail.ru>
2022-06-15 16:08:24 +0300

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from where_to_go import views
+from where_to_go.settings import STATIC_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
