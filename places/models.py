a8f269e3e6e3a75c00cf5713e127a20e04c25bc6
Alexey T <troyanskiy-a@mail.ru>
2022-08-06 09:46:15 +0300

    Add model for image

--- places/models.py
+++ places/models.py
@@ -10,3 +10,11 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title
