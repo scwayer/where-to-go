d13c3b6c6c0d2d9e91a84728419673b63b753a5f
atroyansky <a.troyansky@infomaximum.com>
2022-08-25 23:06:32 +0300

    Add wysiwyg-editor

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название")
     description_short = models.CharField(max_length=255, verbose_name="Короткое описание")
-    description_long = models.TextField(verbose_name="Описание")
+    description_long = HTMLField(default="", verbose_name="Описание")
     coordinates_lng = models.FloatField(verbose_name="Долгота")
     coordinates_lat = models.FloatField(verbose_name="Широта")
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('places/<int:id>/', views.get_place, name='json_response'),
+    path('tinymce/', include('tinymce.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
